41c201c981e60935008825280a6d6a0279e7df5b
DestructoDemon <90038122+DestructoDemon@users.noreply.github.com>
2024-01-16 11:33:25 +0530

    Completed the layout and updates

--- ToDo/index.js
+++ ToDo/index.js
@@ -5,13 +5,17 @@ function addToDo(){
     const desc = document.getElementById("desc").value
     const child = document.createElement("div")
     child.setAttribute("id",count)
+    child.setAttribute("class","newToDo")
     const grandchild1 = document.createElement("div")
-    grandchild1.innerHTML = title
+    grandchild1.innerHTML = "ToDo : "+title
+    grandchild1.setAttribute("class","newBox")
     const grandchild2 = document.createElement("div")
-    grandchild2.innerHTML = desc
+    grandchild2.innerHTML = "Detail : "+desc
+    grandchild2.setAttribute("class","newBox")
     const grandchild3 = document.createElement("button")
     grandchild3.innerHTML = "mark done"
     grandchild3.setAttribute("onclick", `done(${count})`)
+    grandchild3.setAttribute("class","listButton")
     child.appendChild(grandchild1)
     child.appendChild(grandchild2)
     child.appendChild(grandchild3)
