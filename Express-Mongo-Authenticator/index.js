737354e57cf2138199f944846942684f5a804592
DestructoDemon <90038122+DestructoDemon@users.noreply.github.com>
2024-01-17 14:34:33 +0530

    added check feature for determining if user is already present

--- Express-Mongo-Authenticator/index.js
+++ Express-Mongo-Authenticator/index.js
@@ -4,17 +4,29 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-app.post("/signup",function(req,res){
+app.post("/signup",async function(req,res){
     const username = req.body.username
     const password = req.body.password
-    Admin.create({
+    const check = await Admin.findOne({
         username : username,
         password : password
-    }).then(function(){
+    })
+    if(check){
+        res.json({
+            msg : "User already present"
+        })
+    }
+    else{
+        await Admin.create({
+            username : username,
+            password : password
+        })
         res.json({
             msg : "Admin Created Successfully"
         })
-    })
+    }
+    
+    
 })
 
 app.listen(3000)
