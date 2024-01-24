6bc0d6e64b5f61eb790c76d651c01c9ab9d6b69f
DestructoDemon <90038122+DestructoDemon@users.noreply.github.com>
2024-01-24 10:01:20 +0530

    created express paths

--- ToDo(MERN)/backend/index.js
+++ ToDo(MERN)/backend/index.js
@@ -3,4 +3,16 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+app.post("/todo", function(req,res){
+
+})
+
+app.get("/fetchTodo", function(req,res){
+
+})
+
+app.put("/done", function(req,res){
+    
+})
+
 app.listen(3000)
