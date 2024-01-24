45c4ff4a2bb9ffb8dd4c9fffbd92d4c0b470db3d
DestructoDemon <90038122+DestructoDemon@users.noreply.github.com>
2024-01-24 10:22:46 +0530

    imported the validation from types.js and implemented in index.js

--- ToDo(MERN)/backend/index.js
+++ ToDo(MERN)/backend/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
+const {createTodo, updateTodo} = require("./types")
 
 const app = express()
 app.use(express.json())
 
 app.post("/todo", function(req,res){
+    const todo = req.body
+    const parse = createTodo.safeParse(todo)
+    if(!parse.success){
+        res.status(411).json({
+            msg : "You sent wrong input"
+        })
+        return
+    }   
+
 
 })
 
@@ -12,7 +22,16 @@ app.get("/fetchTodo", function(req,res){
 })
 
 app.put("/done", function(req,res){
-    
+    const todo = req.body
+    const parse = updateTodo.safeParse(todo)
+    if(!parse.success){
+        res.status(411).json({
+            msg : "You sent wrong input"
+        })
+        return
+    }
+
+
 })
 
 app.listen(3000)
