747883ca0ed10940cdd7f1bfd343082bbbf4167b
DestructoDemon <90038122+DestructoDemon@users.noreply.github.com>
2024-01-25 15:15:24 +0530

    Added mongoDB connection string

--- ToDo(MERN)/backend/db.js
+++ ToDo(MERN)/backend/db.js
@@ -1,7 +1,7 @@
 // mongo db database for the app
 const mongoose = require("mongoose")
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://vansh:1234@cluster1.k821jap.mongodb.net/Todo")
 
 const data = new mongoose.Schema({
     title : String,
